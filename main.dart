99475d95eece31d6478dc2f3eb400f7a62b4b462
Platypus84 <mailtojulian@gmx.de>
2025-06-03 11:20:22 +0200

    create_variables

--- main.dart
+++ main.dart
@@ -1,3 +1,14 @@
 void main() {
   String gdStr = "Guten";
+  String dayStr = "Tag";
+  String worldStr = "Welt";
+  int d = 20;
+  String timeStr = "Zeit";
+  int seven = 7;
+  int twelve = 12;
+  String e = "!";
+  int a = 24;
+  String c = "Schöner";
+  String morning = "Morgen";
+  String s = "Heute";
 }
