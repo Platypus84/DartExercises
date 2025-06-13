f385eb5ee24882b321b0a45c80e1bd8ccf656cfa
Platypus84 <mailtojulian@gmx.de>
2025-06-13 11:58:02 +0200

    3.4.5_Aufgabe1und2_AppTeilnehmer

---
+++ 3.4.5_Aufgabe1und2_AppTeilnehmer/main.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+
+void main() {
+  cleanTerminal();
+  List<int> points = [4, 5, 4, 2, 6, 6, 3];
+  List<String> names = [
+    'Julietta',
+    'Benjamino',
+    'Hans-Günther',
+    'Evalinea',
+    'Fiona',
+    'Gregory',
+    'Leonhart',
+  ];
+  int sumPoints = 0;
+  int amountPoints = points.length;
+
+  print('\n' + 'Aufgabe 1 - Lösung 1:');
+
+  for (String name in names) {
+    print(name);
+  }
+  print('\n');
+  print('\n' + 'Aufgabe 1 - Lösung 2:');
+  names.forEach(print);
+
+  print('\n' + 'Aufgabe 2 - Lösung:');
+  for (int point in points) {
+    sumPoints += point;
+  }
+
+  double avgPoints = sumPoints / amountPoints;
+  print('Das Ergebnis ist: $avgPoints' + '\n');
+}
+
+void cleanTerminal() {
+  print(Process.runSync("clear", [], runInShell: true).stdout);
+}
---
+++ reminder.dart
@@ -0,0 +1,14 @@
+void main() {
+  // var name = print;
+
+  // print(name);
+
+  // name('hallo welt');
+
+  var name = () => 7;
+  print(name());
+}
+
+int getNumber() {
+  return 7;
+}
