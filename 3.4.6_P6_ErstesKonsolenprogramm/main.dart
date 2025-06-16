1e5b7cd9617f095a07a716e3136a29bec8ff6b92
Platypus84 <mailtojulian@gmx.de>
2025-06-16 13:09:57 +0200

    edit01_3.4.5_Calculator

--- 3.4.6_P6_ErstesKonsolenprogramm/main.dart
+++ 3.4.6_P6_ErstesKonsolenprogramm/main.dart
@@ -6,10 +6,23 @@ void main() {
 }
 
 void calculator() {
-  late double number01;
-  late double number02;
+  late double operand01;
+  late double operand02;
   late double result;
-  Map<int,String> calcTypes = {1:'Addition', 2:'Subtraktion', 3:'Multiplikation', 4:'Division'};
+
+  Map<int, String> calcTypes = {
+    1: 'Addition',
+    2: 'Subtraktion',
+    3: 'Multiplikation',
+    4: 'Division',
+  };
+
+  Map<int, List<String>> calcOperands = {
+    1: ['Summand 1', 'Summand 2'],
+    2: ['Minuend', 'Subtrahend'],
+    3: ['Multiplikator', 'Multiplikand'],
+    4: ['Dividend', 'Divisor'],
+  };
 
   print(
     '\n' + 'Dein Taschenrechner - Wähle eine Rechenart mit einer Ziffer aus:',
@@ -25,30 +38,30 @@ void calculator() {
   int calculus = int.parse(stdin.readLineSync() ?? '0');
 
   // Ausgabe der gewählten Rechenart
-  print('\n' + 'Du hast <Platzhalter> als Rechenart gewählt.');
+  print('\n' + 'Du hast ${calcTypes[calculus]} als Rechenart gewählt.');
 
   // Eingabe der Zahlen
-  print('\n' + 'Gib die erste Zahl ein:');
-  number01 = double.parse(stdin.readLineSync() ?? '0');
+  print('\n' + 'Gib die erste Zahl (${calcOperands[calculus]?.first}) ein:');
+  operand01 = double.parse(stdin.readLineSync() ?? '0');
 
   if (calculus != 6) {
-    print('\n' + 'Gib die zweite Zahl ein:');
-    number02 = double.parse(stdin.readLineSync() ?? '0');
+    print('\n' + 'Gib die zweite Zahl (${calcOperands[calculus]?.last}) ein:');
+    operand02 = double.parse(stdin.readLineSync() ?? '0');
   }
 
   switch (calculus) {
     case 1:
-      result = addition(number01, number02);
+      result = addition(operand01, operand02);
     case 2:
-      result = subtraction(number01, number02);
+      result = subtraction(operand01, operand02);
     case 3:
-      result = multiplication(number01, number02);
+      result = multiplication(operand01, operand02);
     case 4:
-      result = division(number01, number02);
+      result = division(operand01, operand02);
     case 5:
-      result = modulo(number01, number02);
+      result = modulo(operand01, operand02);
     case 6:
-      result = root(number01);
+      result = root(operand01);
     default:
       print('Fehler: Keine Rechenart gewählt');
   }
@@ -57,32 +70,32 @@ void calculator() {
   print('\n' + '- - - - - -');
 }
 
-double addition(num01, num02) {
-  double result = num01 + num02;
+double addition(operand01, operand02) {
+  double result = operand01 + operand02;
   return result;
 }
 
-double subtraction(num01, num02) {
-  double result = num01 - num02;
+double subtraction(operand01, operand02) {
+  double result = operand01 - operand02;
   return result;
 }
 
-double multiplication(num01, num02) {
-  double result = num01 * num02;
+double multiplication(operand01, operand02) {
+  double result = operand01 * operand02;
   return result;
 }
 
-double division(num01, num02) {
-  double result = num01 / num02;
+double division(operand01, operand02) {
+  double result = operand01 / operand02;
   return result;
 }
 
-double modulo(num01, num02) {
-  double result = num01 % num02;
+double modulo(operand01, operand02) {
+  double result = operand01 % operand02;
   return result;
 }
 
-double root(num01) {
-  double result = sqrt(num01);
+double root(operand01) {
+  double result = sqrt(operand01);
   return result;
 }
