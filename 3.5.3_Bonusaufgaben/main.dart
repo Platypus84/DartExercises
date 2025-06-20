1044d0372ab377768e99b8ec2934aa35941dd3ba
Platypus84 <mailtojulian@gmx.de>
2025-06-20 12:48:52 +0200

    edit01_3.5.3_Bonusaufgaben

--- 3.5.3_Bonusaufgaben/main.dart
+++ 3.5.3_Bonusaufgaben/main.dart
@@ -1,12 +1,12 @@
 main() {
   print('\n' + 'Bonusaufgabe 1 - Zahlenvergleich:');
-  print(countChars(2, 10));
-  print(countChars(89, 3));
-  print(countChars(11, 99));
+  print(compareNumbers(2, 10));
+  print(compareNumbers(89, 3));
+  print(compareNumbers(11, 99));
   print('\n');
 }
 
-int countChars(int number1, int number2) {
+int compareNumbers(int number1, int number2) {
   int result = number1 > number2 ? number1 : number2;
   return result;
 }
