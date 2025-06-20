3fe90ef3e8a803c6d0a20875535c5819921e3ae0
Platypus84 <mailtojulian@gmx.de>
2025-06-20 13:21:01 +0200

    edit02_3.5.3_Bonusaufgaben

--- 3.5.3_Bonusaufgaben/main.dart
+++ 3.5.3_Bonusaufgaben/main.dart
@@ -1,12 +1,46 @@
 main() {
+  List<int> numList1 = [34, 57, 1, 100, -42, 2, 2, 12, 73, -17];
+  List<int> numList2 = [5, 82, 19, 17];
+  List<int> numList3 = [100, -42, 17, 231];
+
   print('\n' + 'Bonusaufgabe 1 - Zahlenvergleich:');
   print(compareNumbers(2, 10));
   print(compareNumbers(89, 3));
   print(compareNumbers(11, 99));
   print('\n');
+
+  print('Bonusaufgabe 2 - Zahlanalyse (Gerade/Ungerade):');
+  print(analyseNumber(2));
+  print(analyseNumber(9));
+  print(analyseNumber(17));
+  print('\n');
+
+  print('Bonusaufgabe 3 - Zahlensumme aus Liste:');
+  print(sumListItems(numList1));
+  print(sumListItems(numList2));
+  print(sumListItems(numList3));
+  print('\n');
 }
 
+// Bonusaufgabe 1 - Zahlenvergleich:
 int compareNumbers(int number1, int number2) {
   int result = number1 > number2 ? number1 : number2;
   return result;
+}
+
+// Bonusaufgabe 2 - Zahlanalyse (Gerade/Ungerade):
+String analyseNumber(int number) {
+  String result = number % 2 == 0
+      ? 'Die Zahl ist gerade.'
+      : 'Die Zahl ist ungerade.';
+  return result;
+}
+
+// Bonusaufgabe 3 - Zahlensumme aus Liste:
+sumListItems(List list) {
+  num sumItems = 0;
+  list.forEach((element) {
+    sumItems += element;
+  });
+  return sumItems;
 }
